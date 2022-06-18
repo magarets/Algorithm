717fc98778829b1b962606a19381f212de218eaa
yoonan <alswn81616@gmail.com>
2022-06-18 14:16:05 +0900

    clear

---
+++ 10870/cmake-build-debug/CMakeFiles/10870.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/10870.dir/main.c.o"
+  "10870.pdb"
+  "10870"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/10870.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
