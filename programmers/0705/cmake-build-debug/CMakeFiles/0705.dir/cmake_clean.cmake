40f04d1ae4fa68355c6a691993b463f559a98b76
yoonan <101767824+magarets@users.noreply.github.com>
2022-10-09 23:40:14 +0900

    create programmers folder

---
+++ programmers/0705/cmake-build-debug/CMakeFiles/0705.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/0705.dir/main.cpp.o"
+  "0705.pdb"
+  "0705"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0705.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ programmers/0705/cmake-build-debug/CMakeFiles/0705.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 0705.
+# This may be replaced when dependencies are built.
