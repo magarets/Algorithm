2f3b626b2fa8155ac35289a6b662c43fc79b93d2
yoonan <101767824+magarets@users.noreply.github.com>
2022-10-11 19:45:32 +0900

    update

---
+++ challenge/yeungjin_coding_contest/Prob_1.c
@@ -0,0 +1,16 @@
+/*#include <stdio.h>
+
+int diff(int MountainHigh, int UserHigh) {
+    return MountainHigh - UserHigh;
+}
+
+int main() {
+    int MountainHigh;
+    int UserHigh;
+
+    scanf("%d %d", &MountainHigh, &UserHigh);
+
+    printf("%d", diff(MountainHigh, UserHigh));
+    return 0;
+}
+
